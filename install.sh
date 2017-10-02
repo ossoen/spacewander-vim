499bb0f37b27f252d4840ab971d37d112ffc5177
spacewander <spacewanderlzx@gmail.com>
2017-10-02 17:45:42 +0800

    modify for neovim

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -ex
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:=$HOME/.config}"
 
 
 BASEDIR="$(dirname "$0")"
@@ -9,19 +11,18 @@ lnif() {
     if [ ! -e "$2" ] ; then
         ln -s "$1" "$2"
     fi
-    if [ -L "$2" ] ; then
-        ln -sf "$1" "$2"
-    fi
 }
 
 sync_dir() {
-    test -d "$HOME"/.vim/"$1" && cp -r "$HOME"/.vim/"$1"/* "$CURRENT_DIR"/"$1"
+    if [ -d "$HOME"/.vim/"$1" ]; then
+       cp -r "$HOME"/.vim/"$1"/* "$CURRENT_DIR"/"$1"
+    fi
 }
 
 echo "backing up current vim config (保存当前vim配置为xx.date的形式)"
 today=$(date +%Y%m%d)
-for i in "$HOME"/.vim "$HOME"/.vimrc "$HOME"/.gvimrc; do [ -e "$i" ] && [ ! -L "$i" ] && mv "$i" "$i"."$today"; done
-for i in "$HOME"/.vim "$HOME"/.vimrc "$HOME"/.gvimrc; do [ -L "$i" ] && unlink "$i" ; done
+for i in "$HOME"/.vim "$HOME"/.vimrc "$HOME"/.gvimrc "$XDG_CONFIG_HOME"/nvim; do [ -e "$i" ] && [ ! -L "$i" ] && mv "$i" "$i"."$today"; done
+for i in "$HOME"/.vim "$HOME"/.vimrc "$HOME"/.gvimrc "$XDG_CONFIG_HOME"/nvim; do [ -L "$i" ] && unlink "$i" ; done
 
 
 echo "同步文件夹"
@@ -31,9 +32,10 @@ sync_dir undo
 
 echo "setting up symlinks (设置链接，将.vim 和 .vimrc 分别链接到 spacewander-vim 和 spacewander-vim/vimrc)"
 echo "所以不要删除上述的文件夹"
+lnif "$CURRENT_DIR"/ "$HOME"/.vim
+lnif "$CURRENT_DIR"/ "$XDG_CONFIG_HOME"/nvim
 lnif "$CURRENT_DIR"/vimrc "$HOME"/.vimrc
 lnif "$CURRENT_DIR"/vimrc "$HOME"/.gvimrc
-lnif "$CURRENT_DIR"/ "$HOME"/.vim
 
 if [ ! -e "$CURRENT_DIR"/neobundle.vim ]; then
     echo "Installing NeoBundle"
